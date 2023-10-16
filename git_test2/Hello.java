18f4f6b41855df170a51b541e0870f9cc0678cc4
Sujeong Kim <lewis14081626@gmail.com>
2023-10-16 22:30:18 +0900

    Hello 클래스 수정

--- git_test2/Hello.java
+++ git_test2/Hello.java
@@ -1,6 +1,16 @@
 package git_test2;
 
 public class Hello {
+    String message;
+
+    public Hello() {
+        this("안녕하세요");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
+    
     public void say() {
         System.out.println("hello world");
     }
