848441ee7b96b1cfb84652fc35fa154ced99f946
Sujeong Kim <lewis14081626@gmail.com>
2023-10-16 22:35:46 +0900

    화면 출력 방법 수정

--- git_test2B/git_test2/Hello.java
+++ git_test2B/git_test2/Hello.java
@@ -2,7 +2,8 @@ package git_test2B.git_test2;
 
 public class Hello {
     String message = "안녕하세요";
-    public void say() {
-        System.out.println(message);
+
+    public String say() {
+        return message;
     }
 }
