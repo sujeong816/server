ca41f39d1ca33e657c457a9c7e2bf30bcadb5535
Sujeong Kim <lewis14081626@gmail.com>
2023-10-17 21:56:10 +0900

    201014099-1

---
+++ a201014099/studentA/git_exam2/Hello.java
@@ -0,0 +1,8 @@
+package a201014099.studentA.git_exam2;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("201014099");
+        System.out.println("홍길동");
+    }
+}
